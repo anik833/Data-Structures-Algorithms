11ef5f4c903084270861db910cc35f88cfdba162
Anik Kumar Paul <anikkumarpaul833@gmail.com>
2026-03-21 13:55:44 +0600

    Sort all the files

---
+++ Data Structures/Linked List/02_Insert Operation in Singly Linked List/B_insert_at_head.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node* next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_head(Node* &head, int val)
+{
+    Node* newhead = new Node(val);
+    newhead->next = head;
+    head = newhead;
+}
+
+void print_linked_list(Node* head)
+{
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+    
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    insert_at_head(head,100);
+    insert_at_head(head,200);
+
+    print_linked_list(head);
+    
+
+    
+
+
+
+
+
+    // Node c;
+    // c.val = 10;
+    // c.next = NULL;
+    // cout << c.val; 
+    
+    return 0;
+}
